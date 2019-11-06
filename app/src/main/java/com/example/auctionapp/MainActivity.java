f599dbdc0984f5984c84bc3562ba75c0164dee51
Sillden <45406564+Sillden@users.noreply.github.com>
2019-11-06 18:47:47 +0100

    Przejścia pomiędzy Activity

--- app/src/main/java/com/example/auctionapp/MainActivity.java
+++ app/src/main/java/com/example/auctionapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.auctionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void OpenLogin (View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+    public void OpenRegister (View view){
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
