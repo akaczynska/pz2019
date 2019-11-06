af04d14a3b12ed1f2069ea74a359a7789afbcfb0
Sillden <45406564+Sillden@users.noreply.github.com>
2019-11-06 18:31:14 +0100

    Model Produktu (test)

---
+++ app/src/main/java/com/example/auctionapp/Adapters/ProductAdapter.java
@@ -0,0 +1,5 @@
+package com.example.auctionapp.Adapters;
+
+public class ProductAdapter {
+
+}
---
+++ app/src/main/java/com/example/auctionapp/Model/ProductInformation.java
@@ -0,0 +1,47 @@
+package com.example.auctionapp.Model;
+
+public class ProductInformation {
+    private String productName;
+    private String productPrice;
+    private String productTime;
+    private String imageURL;
+
+    public ProductInformation(String productName, String productPrice, String productTime, String imageURL) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productTime = productTime;
+        this.imageURL = imageURL;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductTime() {
+        return productTime;
+    }
+
+    public void setProductTime(String productTime) {
+        this.productTime = productTime;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
