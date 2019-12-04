741976c2de73a79e3922e042bbe44749856e1b8d
akaczynska <kaczynska.aleksandra98@gmail.com>
2019-12-04 12:23:19 +0100

    database connection

--- app/src/main/java/com/example/auctionapp/MainActivity.java
+++ app/src/main/java/com/example/auctionapp/MainActivity.java
@@ -2,11 +2,21 @@ package com.example.auctionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MainActivity extends AppCompatActivity {
+
+    public Connection con;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +28,30 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openRegister (View view){
         startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+
+    @SuppressLint("NewApi")
+    public Connection connectionClass()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURL = "jdbc:jtds:sqlserver://auctionapp.database.windows.net:1433;DatabaseName=AuctionAppDB;user=kaczynskaa@auctionapp;password=Slash1998;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            connection = DriverManager.getConnection(connectionURL);
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
