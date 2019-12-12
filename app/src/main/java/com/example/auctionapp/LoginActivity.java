7125fad65c5704c2d2b409464421ea7a9157e3b0
BKudlik <kudi96@gmail.com>
2019-12-12 14:57:39 +0100

    Make login / register buttons actually do stuff in the database.

--- app/src/main/java/com/example/auctionapp/LoginActivity.java
+++ app/src/main/java/com/example/auctionapp/LoginActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import android.os.AsyncTask;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,16 +19,19 @@ public class LoginActivity extends AppCompatActivity {
     Connection con;
     ConnectionClass connectionClass;
     EditText editText_login;
-
+    EditText editText_password;
+    String login;
+    String password;
+    Boolean loginStatus = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_login);
         connectionClass = new ConnectionClass();
         con = connectionClass.getConnection();
-        editText_login = (EditText)findViewById(R.id.editText_login);
+        editText_login = findViewById(R.id.editText_login);
+        editText_password = findViewById(R.id.editText_haslo);
 
-        setContentView(R.layout.activity_login);
     }
 
     public void openAuction (View view) {
@@ -38,16 +42,47 @@ public class LoginActivity extends AppCompatActivity {
 
     //Funkcja sprawdzajaca czy login i haslo poprawne
     public boolean checkLogin(){
+        //Toast.makeText(this,"janush" , Toast.LENGTH_SHORT).show();
         //Check internet access and database connection
-            if (con != null) {
-                //Get login from application
-                //String login = editText_login.getText().toString();
-                return true;
-            }
-             else {
+        try {
+            if (con == null) {
                 Toast.makeText(this, "Check your internet access!", Toast.LENGTH_SHORT).show();
                 return false;
+
+            } else {
+                login = editText_login.getText().toString();
+                password = editText_password.getText().toString();
+                //String query = "DECLARE @Result TINYINT; EXEC @Result = dbo.VerifyAccount @AccountName = 'Brian', @AccountPwd = 'Str0ngP4ssw0rd!'; SELECT @Result 'Status;";
+                String query = "DECLARE @Result TINYINT;\n" +
+                        "\n" +
+                        "EXEC @Result = dbo.VerifyAccount @AccountName = '" + login + "', @AccountPwd = '" + password + "';\n" +
+
+                        "SELECT @Result Status;";
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                if(rs.next())
+                {
+                    if(rs.getInt("Status")==0)
+                    {
+                        loginStatus=true;
+                        Toast.makeText(this,"Login successful", Toast.LENGTH_LONG).show();
+                        con.close();
+                    }
+                    else if(rs.getInt("Status")==1)
+                        Toast.makeText(this,"Login unsuccessful \n Invalid login or password", Toast.LENGTH_LONG).show();
+                }
+               else
+                {
+                    Toast.makeText(this,"Login unsuccessful", Toast.LENGTH_LONG).show();
+                    loginStatus = false;
+                }
             }
         }
+        catch (Exception e)
+        {
+            Toast.makeText(this,e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return loginStatus;
+    }
 
 }
